bf3c6dd968ffa48741ce94215323f450201c8319
Ismaiel <50210234+ismaiel5@users.noreply.github.com>
2024-06-09 15:11:33 +0200

    added pages folder for better naming and structure

--- app/modules/pages.ts
+++ app/modules/pages.ts
@@ -3,4 +3,5 @@ import {html} from 'lit';
 export const pages = {
     home: html`<app-main></app-main>`, 
     about: html`<app-about-us></app-about-us>`, 
+    error: html`<app-error><app-error>`
 }
