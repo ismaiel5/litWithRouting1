2d22057d22fd5ce8cc7aff4304b1132ccd14a63b
Ismaiel <50210234+ismaiel5@users.noreply.github.com>
2024-06-09 15:08:30 +0200

    rename loader which exports all components

---
+++ app/global/component-loader.ts
@@ -0,0 +1,8 @@
+
+// List of the exported components
+export * from '../pages/app-page.ts';
+
+export * from '../pages/app-main.ts';
+export * from '../pages/app-aboutUs.ts';
+export * from '../pages/app-error.ts';
+
--- app/global/main.ts
+++
@@ -1,7 +0,0 @@
-
-
-// List of the exported components
-export * from '../components/app-home.ts';
-export * from '../components/app-page.ts';
-export * from '../components/app-aboutUs.ts';
-export * from '../components/app-main.ts';
